90e2e82146c46a7c75b4d8974a73540dce189d6e
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-09 11:21:24 +0600

    minor style fix

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 const page = async({params}: ParamsWithSearch) => {
   const {id}= await params;
     return (
-    <div className='text-2xl font-karla wrapper page'>
+    <div className='wrapper page'>
         <Header subHeader='mdasad.mash@gmail.com' title='asadmash' userImg='/assets/images/dummy.jpg'/>
-        USER ID: {id}
+         <h1 className='text-2xl font-karla'>USER ID: {id}</h1>
         </div>
   )
 }
