2eedf5388d4a2f7bf9c3c008cb8fefaf35ec87ec
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-09 10:03:58 +0600

    added video upload & record button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,14 @@
+
+
 import React from 'react'
+import Header from '@/components/Header'
 
 const Page = () => {
   return (
-    <div>
+    <main className='wrapper page'>
+      <Header title='All Videos' subHeader='Public Library'/>
       <h1 className="text-2xl font-karla">Welcome to loom</h1>
-    </div>
+    </main>
   )
 }
 
