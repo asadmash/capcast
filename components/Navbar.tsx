5c67c3c59e3cfa52b3323dfb8b82e8935d357214
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-09 10:35:27 +0600

    added navigation router

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,14 @@
+"use client"
 import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from 'next/navigation';
+
 
 const user = {};
 
 const Navbar = () => {
+    const router = useRouter();
     return (
        <header className="navbar">
 <nav>
@@ -14,7 +18,7 @@ const Navbar = () => {
     </Link>
     {user && (
         <figure>
-            <button>
+            <button onClick={() => router.push("/profile/123456")} >
                 <Image src="/assets/images/dummy.jpg" alt="User Image" width={32} height={32} className="rounded-full aspect-square"/>
             </button>
             <button className="cursor-pointer">
