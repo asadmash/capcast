1f8c18a0313572843315d6d73dd408cea38682e0
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-09 11:19:01 +0600

    updated profile page data

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,9 +1,13 @@
+import Header from '@/components/Header';
 import React from 'react'
 
-const page = async({params}: ParamasWithSearch) => {
+const page = async({params}: ParamsWithSearch) => {
   const {id}= await params;
     return (
-    <div>USER ID: {id}</div>
+    <div className='text-2xl font-karla wrapper page'>
+        <Header subHeader='mdasad.mash@gmail.com' title='asadmash' userImg='/assets/images/dummy.jpg'/>
+        USER ID: {id}
+        </div>
   )
 }
 
