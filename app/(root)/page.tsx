1265b1370f4018f5ab7aad625e8ea639f694c687
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-11 15:08:53 +0600

    added videocard component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,17 @@
 
 
 import React from 'react'
-import Header from '@/components/Header'
+import Header from '@/components/Header';
+import VideoCard from '@/components/VideoCard';
 
 const Page = () => {
   return (
     <main className='wrapper page'>
       <Header title='All Videos' subHeader='Public Library'/>
       <h1 className="text-2xl font-karla">Welcome to loom</h1>
+
+      {/* STEP:3 RENDER VIDEO CARD COMPONENET */}
+      <VideoCard/>
     </main>
   )
 }
---
+++ components/VideoCard.tsx
@@ -0,0 +1,10 @@
+// STEP: 01 RUN RAFCE TO CREATE A REACT ARROW COMPONENT
+import React from 'react'
+
+const VideoCard = () => {
+  return (
+    <div>VideoCard</div>
+  )
+}
+
+export default VideoCard
