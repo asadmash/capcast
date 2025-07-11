6e61789375502fb2175bb91c8c6c56ec4f9b36af
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-11 15:15:42 +0600

    passed sample props from page to videocard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,7 +11,18 @@ const Page = () => {
       <h1 className="text-2xl font-karla">Welcome to loom</h1>
 
       {/* STEP:3 RENDER VIDEO CARD COMPONENET */}
-      <VideoCard/>
+      <VideoCard
+      // STEP:4 PASS PROPS TO THE VIDEOCARD COMPONENT
+      id='1'
+      title='CapCast Message - 01 july 2025'
+      thumbnail='assets/samples/thumbnail (1).png'
+      createdAt='2025-07-01'
+      userImg='assets/images/jason.png'
+      username='Jason'
+      views={10}
+      visibility='public'
+      duration={156}
+      />
     </main>
   )
 }
