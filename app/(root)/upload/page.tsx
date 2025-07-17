b91cb20115422fe5315762324724e07364cb0689
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-18 00:48:11 +0600

    added upload page

---
+++ app/(root)/upload/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import FileInput from "@/components/FileInput";
+import FormField from "@/components/FormField";
+import React, { ChangeEvent, useState } from "react";
+
+const Page = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    visibility: "public",
+  });
+  const [error, setError] = useState(null);
+
+  const handleInputChange = (e: ChangeEvent) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  return (
+    <div className="wrapper-md upload-page">
+      <h1>Upload a video</h1>
+      {error && <div className="error-field">{error}</div>}
+
+      <form className="rounded-20 shadow-10 gap-6 w-full flex flex-col px-5 py-7.5">
+        <FormField
+          id="title"
+          label="Title"
+          placeholder="Enter a clear and consise video title"
+          value={formData.title}
+          onChange={handleInputChange}
+        />
+        <FileInput />
+      </form>
+    </div>
+  );
+};
+
+export default Page;
