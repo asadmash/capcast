d2377b1b4b8cf28e8ee4f931507f9aa4017e0d90
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-22 21:01:36 +0600

    added video uploading components to upload page

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -1,36 +1,89 @@
-"use client";
-import FileInput from "@/components/FileInput";
+"use client"; //make the entire component client site because we are using useState
+import FileInput from "@/components/FileInput"; //
 import FormField from "@/components/FormField";
 import React, { ChangeEvent, useState } from "react";
 
 const Page = () => {
+  // State to manage form data
   const [formData, setFormData] = useState({
     title: "",
-    description: "",
-    visibility: "public",
+    desscription: "",
+    visbility: "public",
   });
-  const [error, setError] = useState(null);
 
+  // an object to store the videos
+  const video = {};
+  // an object to store the thumbnails
+
+  const [error, setError] = useState(null); //state declaration for error handleing
+
+  // a function to handle input change
   const handleInputChange = (e: ChangeEvent) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target; //extract the name and value from the target
+
+    // set the name value data to the formData
     setFormData((prevState) => ({ ...prevState, [name]: value }));
   };
 
   return (
     <div className="wrapper-md upload-page">
       <h1>Upload a video</h1>
-      {error && <div className="error-field">{error}</div>}
+      {/* when error become true this div will be show with the error message */}
+      {error && <div className="erro-field">{error}</div>}
+      {/* a form to take user input */}
+      <form className="rounded-20 shadow-10 gap-6 w-full flex flex-col px-5 py-7.5"></form>
+      <FormField
+        id="title"
+        label="Title"
+        placeholder="Enter a clear and concise video title"
+        value={formData.title}
+        onChange={handleInputChange} //on change event listener
+      />
+      <FormField
+        id="Description"
+        label="Description"
+        placeholder="Describe what this video is about"
+        value={formData.desscription}
+        as="textarea"
+        onChange={handleInputChange} //on change event listener
+      />
+      {/* file input element to render file upload componenet */}
+      <FileInput
+        id="video"
+        label="video"
+        accept="video/*"
+        file={video.file}
+        previewUrl={video.previewUrl}
+        inputRef={video.inputRef}
+        onChange={video.handleFileChange}
+        onReset={video.resetFile}
+        type="video"
+      />
 
-      <form className="rounded-20 shadow-10 gap-6 w-full flex flex-col px-5 py-7.5">
-        <FormField
-          id="title"
-          label="Title"
-          placeholder="Enter a clear and consise video title"
-          value={formData.title}
-          onChange={handleInputChange}
-        />
-        <FileInput />
-      </form>
+      {/* input element to render thumbnail upload component */}
+      <FileInput
+        id="thumbnail"
+        label="Thumbnail"
+        accept="image/*"
+        file={thumbnail.file}
+        previewUrl={thumbnail.previewUrl}
+        inputRef={thumbnail.inputRef}
+        onChange={thumbnail.handleFileChange}
+        onReset={thumbnail.resetFile}
+        type="image"
+      />
+
+      <FormField
+        id="visibility"
+        label="Visibility"
+        value={formData.visbility}
+        as="select"
+        options={[
+          { value: "public", label: "Public" },
+          { value: "private", label: "Private" },
+        ]}
+        onChange={handleInputChange} //on change event listener
+      />
     </div>
   );
 };
