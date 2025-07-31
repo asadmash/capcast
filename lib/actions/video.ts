168319f0885ebdfa60725852b0452e321918867c
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-01 02:57:55 +0600

    limit user video upload

--- lib/actions/video.ts
+++ lib/actions/video.ts
@@ -97,8 +97,8 @@ export const getThumbnailUploadUrl = withErrorHandling(
 
 export const saveVideoDetails = withErrorHandling(async (videoDetails: VideoDetails) => {
 const userId = await getSessionUserId();
-
-
+// limit user
+await validateWithArcjet(userId);
 
 await apiFetch(
     `${VIDEO_STREAM_BASE_URL}/${BUNNY_LIBRARY_ID}/videos/${videoDetails.videoId}`,
