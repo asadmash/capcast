00d7cf87e1f6bab58a06723c350fa802d2927290
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-02 02:03:31 +0600

    validate email

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,5 +1,11 @@
+import aj from "@/lib/arcjet";
 import { auth } from "@/lib/auth";
+import { slidingWindow, validateEmail } from "@arcjet/next";
 import { toNextJsHandler } from "better-auth/next-js";
 
 export const {GET, POST} = toNextJsHandler(auth.handler)
 
+// Email Validation
+const emailValidation = aj.withRule(
+    validateEmail({mode: 'LIVE', block: ['DISPOSABLE', 'INVALID', 'NO_MX_RECORDS']})
+)
